934b54c1e4b94c2a7f65dde17b42c1749debf648
itirbit <twmw@mail.ru>
2021-02-05 11:44:33 +0000

    latch c++20

---
+++ cpp20/latches/bossWorkers.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <mutex>
+#include <latch>
+#include <thread>
+
+std::latch workDone(6);
+std::latch go Home(1);
+
+void syncOut(const std::string s)
+{
+	std::lock_guard<std::mutex> lo(coutMutex);
+	std::cout << s;
+}
+
+class Worker
+{
+public:
+	Worker(std::string n): name(n) {};
+	void operator()()
+	{
+		syncOut(name + ": Work done!\n");
+		workDone.count_down();
+
+		goHome.wait();
+		syncOut(name + ": Good bye!\n");
+	}
+private:
+	std::string name;
+};
+
+int main()
+{
+	std::cout << '\n';
+	std::cout << "BOSS: START WORKING! \n";
+	Worker herb("  Herb");
+	std::thread herbWork(herb);
+
+	Worker scott("   Scott");
+	std::thread scottWork(scott);
+
+	Worker bjarne("       Bjarne");
+	std::thread bjarneWork(bjarne);
+
+	Worker andrei("            Andrei");
+	std::thread andreWork(andrei);
+
+	Worker andrew("               Andrew");
+	std::thread andrewWork(andre);
+
+	Worker david("                   David");
+	std::thread davidWork(david);
+
+	workDone.wait();
+	std::cout << '\n';
+	goHome.count_down();
+	std::cout << "BOSS: GO HOME!\ n";
+
+	herbWork.join();
+	scottWork.join();
+	bjarneWork.join();
+	andreiWork.join();
+	davidWork.join();
+
+}
+
+
+
+
+
+
+
+
+

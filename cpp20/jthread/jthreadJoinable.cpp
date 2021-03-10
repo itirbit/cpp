3df5b82cbb56bcf498bbe88b86fe192e97fa4a6b
itirbit <twmw@mail.ru>
2021-03-10 14:31:43 +0000

    add jthread

---
+++ cpp20/jthread/jthreadJoinable.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <thread>
+
+int main()
+{
+	std::cout << '\n';
+	std::cout << std::boolalpha;
+	std::jthread thr{[](){ std::cout<<"Joinable std::jthread\n";}};
+	std::cout << "thr.joinable(): " << thr.joinable() << '\n';
+	std::cout <<'\n';
+}
